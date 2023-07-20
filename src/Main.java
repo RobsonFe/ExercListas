6d00befa8614294d023885dbdcadf87fa5ce016a
Robson Ferreira <robson_20ferreira@hotmail.com>
2023-07-19 22:10:53 -0300

    ExercicioListas

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> nomes = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Cadastro de Pessoas");
+
+        String nome;
+        do {
+            System.out.println("Digite um nome para cadastrar ou digite '0' para sair ");
+            nome = scan.nextLine();
+
+            if (!nome.equals("0")) {
+                nomes.add(nome);
+            }
+
+        } while (!nome.equals("0"));
+
+        System.out.println("A quantidade de pessoas cadastradas são: " + nomes.size());
+        System.out.println();
+        System.out.println("Quantidade de pessoas cadastradas");
+
+        for (String nomeCadastrado : nomes) {
+            System.out.println("Nome: " + nomeCadastrado);
+        }
+    }
+}
